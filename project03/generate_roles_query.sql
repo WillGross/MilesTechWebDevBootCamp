270f0c69b7001ee715912bb01564b30278d8ba4f
WillGross <wlgross96@gmail.com>
2019-03-07 21:53:11 -0500

    cleaned up roles querey

--- project03/generate_roles_query.sql
+++ project03/generate_roles_query.sql
@@ -3,19 +3,19 @@
 --3/7/19
 
 --query creates a table that represents a create statment to generate my roles table sans the identity and primary key properties of 'id'
-SELECT DISTINCT CONCAT('CREATE TABLE ', TABLE_NAME, ' (') FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME LIKE 'roles'
+SELECT DISTINCT CONCAT('CREATE TABLE ', TABLE_NAME, ' (') FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'roles'
 UNION ALL
 SELECT CONCAT(
   CONCAT('  ', COLUMN_NAME, ' ', DATA_TYPE), 
   CASE 
-    WHEN DATA_TYPE like 'varchar' THEN CONCAT( '(', CHARACTER_MAXIMUM_LENGTH, ')')
+    WHEN DATA_TYPE = 'varchar' THEN CONCAT( '(', CHARACTER_MAXIMUM_LENGTH, ')')
   END
   ,
   CASE
-	WHEN COLUMN_NAME != 'description' THEN ','
-	ELSE ')'
+	WHEN COLUMN_NAME = 'description' THEN ');'
+	ELSE ','
   END	
-) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME LIKE 'roles'
+) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'roles'
 ;
 
 
